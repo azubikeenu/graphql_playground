e271c93897c38e7643c01a360d7b64a8e3a296a8
Enu Azubike <enuazubike88@gmail.com>
2022-03-17 12:41:36 +0100

    implemented graphql queries and established bidrectional relationships between types using in-memory datasource

--- graphql_basics/src/index.js
+++ graphql_basics/src/index.js
@@ -1,16 +1,49 @@
 import { GraphQLServer } from 'graphql-yoga';
 
 const users = [
-  { id: 1, name: 'Richard', email: 'enuazubike88@gmail.com', age: 33 },
-  { id: 2, name: 'Carlos', email: 'carlos@gmail.com' },
-  { id: 3, name: 'Mike', email: 'mike@gmail.com' },
+  { id: '1', name: 'Richard', email: 'enuazubike88@gmail.com', age: 33 },
+  { id: '2', name: 'Carlos', email: 'carlos@gmail.com' },
+  { id: '3', name: 'Mike', email: 'mike@gmail.com' },
+];
+
+const posts = [
+  {
+    id: '1',
+    title: 'Post one',
+    text: 'This is the text of post one',
+    published: true,
+    author: '1',
+  },
+  {
+    id: '2',
+    title: 'Post two',
+    text: 'This is the text of post two',
+    published: false,
+    author: '1',
+  },
+  {
+    id: '3',
+    title: 'Post three',
+    text: 'This is the text of post three',
+    published: true,
+    author: '2',
+  },
+];
+
+const comments = [
+  { id: '1', text: `you couldn't have said it better`, post: '1', author: '1' },
+  { id: '2', text: `hahaha thats really funny bro`, post: '1', author: '3' },
+  { id: '3', text: `here we go again`, post: '2', author: '2' },
+  { id: '4', text: `Thats all folks`, post: '2', author: '1' },
 ];
 
 const typeDefs = `
  type Query{
-  users : [User!]!
+  users(query : String) : [User!]!
    me : User!
    post : Post!
+   posts(query : String) : [Post!]!
+   comments : [Comment!]!
  }
 
  type User{
@@ -18,6 +51,8 @@ const typeDefs = `
      name : String!
      email : String!
      age : Int
+     posts : [Post!]!
+     comments:[Comment!]!
  }
 
  type Post{
@@ -25,6 +60,15 @@ const typeDefs = `
      title : String
      text : String
      published : Boolean
+     author : User!
+     comments:[Comment!]!
+
+ }
+ type Comment{
+     id : ID!
+     text : String!
+     post : Post!
+     author : User!
  }
 
 
@@ -39,14 +83,64 @@ const resolvers = {
       };
     },
     post: () => {
-      return {
-        id: 'abc12345',
-        title: 'This is a dummy title',
-        text: 'hello world how are you',
-        published: false,
-      };
+      return posts[posts.length - 1];
     },
-    users: (parent, args, ctx, info) => users,
+    users: (parent, args, ctx, info) => {
+      if (!args.query) {
+        return users;
+      }
+      return users.filter((user) =>
+        user.name.toLowerCase().includes(args.query.toLowerCase())
+      );
+    },
+
+    posts: (parent, args, ctx, info) => {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter(
+        (post) =>
+          post.title.toLowerCase().includes(args.query) ||
+          post.text.toLowerCase().includes(args.query)
+      );
+    },
+    comments:(parent ,args , ctx , info) =>{
+       return comments;
+    }
+  },
+  /*Establish relationships between fields */
+  Post: {
+    // relationship between author and the post field in the post type def
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    // relationship between comments and the post field in the post type def
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
+  },
+  User: {
+    // relationship between posts and the user field in the user type def
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => post.author === parent.id);
+    },
+    // relationship between comments and the user field in the user type def
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => comment.author === parent.id);
+    },
+  },
+
+  Comment: {
+    // relationship between comment and the author field in the comment type def
+     author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    // relationship between comment and the post field in the comment type def
+    post(parent, args, ctx, info) {
+      return posts.find((post) => post.id === parent.post);
+    },
   },
 };
 
